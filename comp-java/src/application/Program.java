354e7d313a0e20272db861bad9d1bf4fb94a6299
Lucas da Silva <lucas.dasilva09@hotmail.com>
2025-01-18 23:46:37 -0300

    Composition, employee salary utilities

---
+++ comp-java/src/application/Program.java
@@ -0,0 +1,61 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException { // ESTE MÉTODO PODE GERAR ESTE TIPO DE EXCESSAO: 'throws ParseException'
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);	
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Enter department's name: "); // "INFORME O NOME DO DEPARTAMENTO: "	
+		String departmentName = sc.nextLine();
+		System.out.println("Enter worker data: "); // "
+		System.out.print("Name: ");
+		String workerName = sc.nextLine();
+		System.out.print("Level: ");
+		String workerLevel = sc.nextLine();
+		System.out.print("Base salary: ");
+		double baseSalary = sc.nextDouble();
+		// OBJETO EM MEMÓRIA
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+		
+		System.out.print("How many contracts to this worker? "); // "QUANTOS CONTRATOS TEM ESTE TRABALHADOR?"
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println("Enter contract #" + i + " data:");
+			System.out.print("Date (DD/MM/YYYY): ");
+			Date contractDate = sdf.parse(sc.next());
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			System.out.print("Duration (hours): ");
+			int hours = sc.nextInt();
+			HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+			worker.addContract(contract);
+		}
+		
+		System.out.println();
+		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+		String monthAndYear = sc.next();
+		int month = Integer.parseInt(monthAndYear.substring(0, 2)); // RECORTANDO STRING, GERANDO SUB STRING COM OS DOIS DIGITOS
+		int year = Integer.parseInt(monthAndYear.substring(3));	// RECORTANDO STRING, DO 3 EM DIANTE
+		System.out.println("Name: " + worker.getName());
+		System.out.println("Department: " + worker.getDepartment().getName());
+		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
+		
+		sc.close();
+	}
+
+}
